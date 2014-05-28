92aa0f7bdadf151cf9f3d70de3a513ce7990106b
hyeluri <hyeluri@gmail.com>
2014-05-28 09:27:43 -0700

    trying to fix the complicated tweets bug

--- routes/nexmo.js
+++ routes/nexmo.js
@@ -5,7 +5,10 @@ var nexmo = require('nexmoapi').Nexmo;
 
 var sender = new nexmo(process.env.NEXMO_KEY || ENV.NEXMO_KEY, process.env.NEXMO_SECRET || ENV.NEXMO_SECRET);
 
-module.exports = Promise.promisify(sender.send.bind(sender));
+module.exports = Promise.promisify(function(a, b, c){
+  c = (c.length < 150) ? c : c.slice(0,150) + '...';
+  sender.send(a, b, c);
+});
 
 
 
