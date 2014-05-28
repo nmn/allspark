f2a20e9b01f0c7b76a54edaf3b5b0ba5b9842ae0
hyeluri <hyeluri@gmail.com>
2014-05-28 09:31:20 -0700

    trying to fix number of tweets, promise bug

--- routes/nexmo.js
+++ routes/nexmo.js
@@ -1,14 +1,17 @@
 var Promise = require('bluebird');
 var nexmo = require('nexmoapi').Nexmo;
 
+
+
 //var ENV = require('../env.js');
 
 var sender = new nexmo(process.env.NEXMO_KEY || ENV.NEXMO_KEY, process.env.NEXMO_SECRET || ENV.NEXMO_SECRET);
+var send = Promise.promisify(sender.send.bind(sender));
 
-module.exports = Promise.promisify(function(a, b, c){
+module.exports = function(a, b, c){
   c = (c.length < 150) ? c : c.slice(0,150) + '...';
-  sender.send(a, b, c);
-});
+  return send(a, b, c);
+};
 
 
 
