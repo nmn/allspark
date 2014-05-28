82184af5557e15493157a82c95f32051c079757f
Naman Goel <naman34@gmail.com>
2014-05-28 11:18:15 -0700

    added some styling

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+//app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret:"namanLikesPakwan"}));
 app.use(passport.initialize());
